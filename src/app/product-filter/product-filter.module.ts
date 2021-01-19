dd26cb7f95b0f0a4729b85f50ef77618d1452b29
hansschenker <gian.schenker@gmail.com>
2021-01-19 17:44:19 +0100

    add rxjs-operators

--- src/app/product-filter/product-filter.module.ts
+++ src/app/product-filter/product-filter.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ProductFilterRoutingModule } from './product-filter-routing.module';
 import { ProductFilterComponent } from './product-filter.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 
 @NgModule({
-  declarations: [ProductFilterComponent, ProductListComponent],
+  declarations: [ProductFilterComponent, ProductListComponent, ProductFormComponent, ProductEditComponent],
   imports: [
     CommonModule,
     ProductFilterRoutingModule
   ],
-  exports: [ProductFilterComponent, ProductListComponent]
+  exports: [ProductFilterComponent, ProductListComponent, ProductFormComponent, ProductEditComponent]
 })
 export class ProductFilterModule { }
